7dd9a2241dd582a4f1371d8548426ab517a6452b
Dante <dantealioto@yahoo.it>
2022-09-05 19:31:36 -0300

    subiendo archivos

---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ItemCount from "./ItemCount-";
+
+const ItemListContainer = ({greeting}) => {
+    return(
+        <div>
+            <p>{greeting}, Tutora! </p>
+            <hr />
+            <ItemCount stock = {5} initial = {1} onAdd = {0}/>
+        </div>
+    )
+}
+
+export default ItemListContainer;
